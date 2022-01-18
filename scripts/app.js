3cf4bfa640dba6f26169446083e861cb770e65d7
farbod-mahdian <97865578+farbod-mahdian@users.noreply.github.com>
2022-01-17 19:25:12 -0500

    Update contact link

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,7 @@ const websiteBtn = document.getElementById('websiteBtn');
 
 saveBtn.addEventListener('click', () => {
   window.open(
-    'https://drive.google.com/file/d/1XWESM8a9O0ezrO6yS2_W48TyEZpoFu9U/view?usp=sharing'
+    'https://farbod-mahdian.github.io/VahidShahsavan.vcf'
   );
 });
 
